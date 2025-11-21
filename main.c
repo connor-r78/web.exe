c360f322911398b4ba7d096740ce6ae124afb58f
Connor Rakov <connorrakov@gmail.com>
2025-11-21 16:46:16 -0500

    Skip Past the DOS Portion

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FILE_NOT_FOUND -1
+
+void moveOffset(int* offset, int dest)
+{
+  *offset += ( dest - *offset); 
+}
+
 int newEXEHeader(char* data)
 {
-  int a = data[0];
-  int b = data[1];
+  unsigned char a = data[0];
+  unsigned char b = data[1];
   return b * 256 + a; 
 }
 
@@ -12,12 +19,19 @@ int main()
 {
   FILE* exe;
   exe = fopen("./tests/test.exe", "r");
+
+  if ( !exe ) return FILE_NOT_FOUND;
   
-  char* data = (char*) malloc(3);
-  
-  int offset = 0;
+  char* data = (char*) malloc(3);  
+  int* offset = (int*) malloc(sizeof(int));
+
+  *offset = 0x0;
   while ( fgets(data, 3, exe) ) {
-    if ( offset == 0x3C ) printf("%d\n", newEXEHeader(data));
-    offset += 2;
+    if ( *offset == 0x3C ) moveOffset(offset, newEXEHeader(data));
+    printf("%X\n", *offset);
+    *offset += 2;
   }
+
+  free(data);
+  free(offset);
 }
