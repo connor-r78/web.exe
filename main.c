10cf2b62d5861099a668a3e19f9ef176074f0d62
Connor Rakov <connorrakov@gmail.com>
2025-11-20 21:34:16 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int newEXEHeader(char* data)
+{
+  int a = data[0];
+  int b = data[1];
+  return b * 256 + a; 
+}
+
+int main()
+{
+  FILE* exe;
+  exe = fopen("test.exe", "r");
+  
+  char* data = (char*) malloc(3);
+  
+  int offset = 0;
+  while ( fgets(data, 3, exe) ) {
+    if ( offset == 0x3C ) printf("%d\n", newEXEHeader(data));
+    offset += 2;
+  }
+}
