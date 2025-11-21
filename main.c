821b32a8020dd9b60dce6b5932581ff925630976
Connor Rakov <connorrakov@gmail.com>
2025-11-21 17:03:09 -0500

    Eliminating Magic Numbers

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define HEX_BASE 16
+
+#define DATA_SIZE 2
+#define DATA_SIZE_NULL_TERM DATA_SIZE + 1
 #define FILE_NOT_FOUND -1
+#define STARTING_OFFSET 0x0
+
+#define PE_HEADER_LOC 0x3C
 
 void moveOffset(int* offset, int dest)
 {
   *offset += ( dest - *offset); 
 }
 
-int newEXEHeader(char* data)
+int getPEHeader(char* data)
 {
   unsigned char a = data[0];
   unsigned char b = data[1];
-  return b * 256 + a; 
+  return b * HEX_BASE ^ 2 + a; 
 }
 
 int main()
@@ -22,14 +29,14 @@ int main()
 
   if ( !exe ) return FILE_NOT_FOUND;
   
-  char* data = (char*) malloc(3);  
+  char* data = (char*) malloc(DATA_SIZE_NULL_TERM);  
   int* offset = (int*) malloc(sizeof(int));
 
-  *offset = 0x0;
-  while ( fgets(data, 3, exe) ) {
-    if ( *offset == 0x3C ) moveOffset(offset, newEXEHeader(data));
+  *offset = STARTING_OFFSET;
+  while ( fgets(data, DATA_SIZE_NULL_TERM, exe) ) {
+    if ( *offset == PE_HEADER_LOC ) moveOffset(offset, getPEHeader(data));
     printf("%X\n", *offset);
-    *offset += 2;
+    *offset += DATA_SIZE;
   }
 
   free(data);
